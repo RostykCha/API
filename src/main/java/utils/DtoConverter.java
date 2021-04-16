e80aee030de423b6ee2a96d6d2e61fe5a43f50a1
Rostislav Chabria <51484396+RostykCha@users.noreply.github.com>
2021-04-16 16:32:13 +0300

    converter

--- src/main/java/utils/DtoConverter.java
+++ src/main/java/utils/DtoConverter.java
@@ -1,11 +1,38 @@
 package utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class DtoConverter {
+    static ObjectMapper mapper = new ObjectMapper();
 
-//    public static Object objectToStringJson(){
-//
-//    }
+    public static JsonObject stringToJsonObject(String json) {
+        return new JsonParser().parse(json).getAsJsonObject();
+    }
 
+    public static String objectToJsonString(Object object) {
+        String jsonStr = null;
+        try {
+            jsonStr = mapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonStr;
+    }
 
+    public static Map stringToMap(String json) {
+        Map map = null;
+        try {
+            map = mapper.readValue(json, Map.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 
 }
